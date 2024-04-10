40320ad54053dba6b7363d96adb6320ef174f49f
Veena <veena14cs@gmail.com>
2024-04-10 18:17:45 +0530

    Fixed textwatcher

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.text.Editable
+import android.text.TextUtils
 import android.text.TextWatcher
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import androidx.appcompat.app.AppCompatActivity
@@ -11,7 +15,8 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.viewmodel.SuggestionViewModel
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), TextWatcher {
     private lateinit var autoCompleteTextView: AutoCompleteTextView
     private lateinit var suggestionViewModel: SuggestionViewModel
 
@@ -22,27 +27,30 @@ class MainActivity : AppCompatActivity() {
         autoCompleteTextView = findViewById(R.id.autoCompleteTextview)
         suggestionViewModel = ViewModelProvider(this).get(SuggestionViewModel::class.java)
 
-        autoCompleteTextView.addTextChangedListener { object : TextWatcher{
-            override fun afterTextChanged(p0: Editable?) {
-                TODO("Not yet implemented")
-            }
+        autoCompleteTextView.addTextChangedListener(this)
+    }
 
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                TODO("Not yet implemented")
-            }
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        Log.d("Typing...",""+1)
+    }
 
-            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                suggestionViewModel.getAutoCompleteSuggestions(s.toString()).observe(this@MainActivity,
-                 Observer { suggestions ->
-                        suggestions?.let{
-                            val adapter = ArrayAdapter<String>(this@MainActivity,
-                                android.R.layout.simple_dropdown_item_1line,it.map{item ->
-                                    item.login
-                                })
-                            autoCompleteTextView.setAdapter(adapter)
-                        }
-                    })
-            }
-        } }
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        Log.d("Typing...",""+2)
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+        Log.d("Typing...",""+3)
+        suggestionViewModel.getAutoCompleteSuggestions(p0.toString())
+            .observe(this@MainActivity,
+                Observer { suggestions ->
+                    suggestions?.let {
+                        val adapter = ArrayAdapter<String>(this@MainActivity,
+                            android.R.layout.simple_dropdown_item_1line,
+                            it.map { item ->
+                                item.login
+                            })
+                        autoCompleteTextView.setAdapter(adapter)
+                    }
+                })
     }
 }
